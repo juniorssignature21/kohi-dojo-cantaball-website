14519c6583e3a06a9d66d7da998ce72e85ec21b8
coded <codesignature19@gmail.com>
2026-07-11 22:02:12 +0100

    made edits 5

--- prj/settings.py
+++ prj/settings.py
@@ -32,7 +32,7 @@ USE_S3 = os.getenv('USE_S3', 'False').lower() in ('1', 'true', 'yes')
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = os.getenv('DEBUG', 'False').lower() in ('1', 'true', 'yes')
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
